7e6fb48627df6244c64df5bdb4d0c0afd40cc923
Akash Singh Thagunna <43631294+thagunna@users.noreply.github.com>
2019-01-02 21:02:59 +0530

    Add files via upload

---
+++ Tic_Tac_Toe.py
@@ -0,0 +1,86 @@
+###############################################################################
+##############                                            #####################
+##############    Tic Tac Toe game  Using Python IDLE     #####################
+##############                                            #####################
+###############################################################################
+
+import random
+
+position = [0, 1, 2,
+            3, 4, 5,
+            6, 7, 8]
+
+def Show():
+    print(position[0], '|', position[1], '|', position[2])
+    print('--------------')
+    print(position[3], '|', position[4], '|', position[5])
+    print('--------------')
+    print(position[6], '|', position[7], '|', position[8])
+
+def check(char, pos1, pos2, pos3):
+    if position[pos1] == char and position[pos2] == char and position[pos3] == char:
+        return 1   #  1 Represents True
+                   #  0 Represents False
+
+def Final_check(char):
+    if check(char, 0, 1, 2):
+        return 1
+    elif check(char, 3, 4, 5):
+        return 1
+    elif check(char, 6, 7, 8):
+        return 1
+    elif check(char, 0, 3, 6):
+        return 1
+    elif check(char, 1, 4, 7):
+        return 1
+    elif check(char, 2, 5, 8):
+        return 1
+    elif check(char, 0, 4, 8):
+        return 1
+    elif check(char, 2, 4, 6):
+        return 1
+    # this Method is for all the possible outcomes for winning the game
+    
+def main_program():
+    while 1:
+        
+        value = int(input("Select any position"))
+
+        if position[value] != 'X' and position[value] != 'O':
+            position[value] = 'X'
+
+            if Final_check('X') == 1:
+                print("""                #####################################
+                ####   Hai!!!    You  Win   #########
+                #####################################""")
+                break
+            
+
+            flag = 1
+
+            while flag:
+                random.seed() # To generate random values 
+                system = random.randint(0,8)
+
+                if position[system] != 'X' and position[system] != 'O':
+                    position[system] = 'O'
+
+                    flag = 0
+
+            if Final_check('O') == 1:
+                print("""                #####################################
+                ####   Hai!!!    You  Lose   ########
+                #####################################""")
+                break
+
+                
+        else:
+            print('This position is already taken')
+
+        Show()
+
+
+if __name__ == '__main__':
+
+    main_program()
+   
