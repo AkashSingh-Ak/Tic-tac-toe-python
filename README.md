9901ff6636ab124d5f4eb03d93a66cb354000e57
Akash Singh Thagunna <43631294+AkashSingh-Ak@users.noreply.github.com>
2020-02-20 19:48:12 +0530

    Modified code

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Tic-tac-toe-python
+# *Tic-tac-toe*
 
-Tic-tac-toe (also known as noughts and crosses or Xs and Os) is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+Tic-tac-toe (also known as noughts and crosses or Xs and Os) is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal  wins the game.
+
+# *Requirements*
+
+```
+import numpy as np
+import random
+```
+
+
+![Output](image1.JPG)
+![](image2.JPG)
--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -5,16 +5,27 @@
 ###############################################################################
 
 import random
+import numpy as np
 
-position = [0, 1, 2,
-            3, 4, 5,
-            6, 7, 8]
 
-def Show():
+position = list(range(0, 9))
+
+
+def Show() -> 'This function is to display the structure of tic tac toe':
+    
+    ## The Structure is similar to this
+
+    #    0 | 1 | 2
+    #   ----------
+    #    3 | 4 | 5
+    #   ----------
+    #    6 | 7 | 8
+    
+    
     print(position[0], '|', position[1], '|', position[2])
-    print('--------------')
+    print('----------')
     print(position[3], '|', position[4], '|', position[5])
-    print('--------------')
+    print('----------')
     print(position[6], '|', position[7], '|', position[8])
 
 def check(char, pos1, pos2, pos3):
@@ -22,7 +33,7 @@ def check(char, pos1, pos2, pos3):
         return 1   #  1 Represents True
                    #  0 Represents False
 
-def Final_check(char):
+def Final_check(char)-> 'this Method is for all the possible outcomes for winning the game':
     if check(char, 0, 1, 2):
         return 1
     elif check(char, 3, 4, 5):
@@ -39,19 +50,20 @@ def Final_check(char):
         return 1
     elif check(char, 2, 4, 6):
         return 1
-    # this Method is for all the possible outcomes for winning the game
     
 def main_program():
     while 1:
         
-        value = int(input("Select any position"))
+        
+        value = int(input("Select any position:  ")) 
+        #Value contains the position entered by user
 
         if position[value] != 'X' and position[value] != 'O':
             position[value] = 'X'
 
             if Final_check('X') == 1:
                 print("""                #####################################
-                ####   Hai!!!    You  Win   #########
+                ####  Congrats  You  Win !! #########
                 #####################################""")
                 break
             
@@ -69,7 +81,7 @@ def main_program():
 
             if Final_check('O') == 1:
                 print("""                #####################################
-                ####   Hai!!!    You  Lose   ########
+                #########   You  Lose!!   ###########
                 #####################################""")
                 break
 
@@ -81,6 +93,11 @@ def main_program():
 
 
 if __name__ == '__main__':
+    
+    print('--------------------------------------------------------------------------------')
+    print("############################# Welcome to Tic Tac Toe ###########################")
+    print('--------------------------------------------------------------------------------')
 
+    Show()
     main_program()
    
